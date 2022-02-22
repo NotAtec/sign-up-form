1c520bb377b8e311f695ec1002603e19ca74675f
Jeroen B <jeroenb04@live.com>
2022-02-22 15:15:14 +0100

    Changed working of button to validate
    Form now does NOT send if passwords do not match

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let matching = false;
+
 function check() {
   const password = document.getElementById('pwd');
   const confirm = document.getElementById('cpwd');
@@ -7,6 +9,7 @@ function check() {
     password.style.border = "2px solid green";
     confirm.style.border = "2px solid green";
     span.innerText = "";
+    matching = true;
   } else {
     password.style.border = "2px solid #b91c1c";
     confirm.style.border = "2px solid #b91c1c";
@@ -14,5 +17,14 @@ function check() {
     span.style.color = "#b91c1c";
     span.style.fontSize = "12px";
     span.style.marginTop = "2px";
+    matching = false;
+  }
+}
+
+function submit_form() {
+  if (!matching) {
+    alert("Check if the passwords match.");
+  } else {
+    document.forms['sign-up'].submit();
   }
 }
